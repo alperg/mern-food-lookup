e528da52976579b9bfce529e30c5560c2c9eff4c
Alper Gokcehan <mail@alperg.com>
2020-05-09 15:23:44 -0400

    Update index.js

--- client/src/components/SelectedFoods/index.js
+++ client/src/components/SelectedFoods/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Icon, Table } from 'semantic-ui-react';
+import { Card, Icon, Table } from "semantic-ui-react";
 
 export default function SelectedFoods(props) {
   const { foods } = props;
